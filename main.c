c3ce5ab4fab5f3f3e4db69685bffa3be18349a96
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-02-02 21:31:23 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+
+    printf("ReturnType  Function_name(Argument List)\n{\n\tFunction_Body;\n\tReturn_Statement;\n}\n\n");
+
+    printf("int sum(int num1, int num2)\n{\n\tint result;\n\tresult=num1+num2;\n\treturn result;\n}\n");
+
+    getch();
+    return 0;
+}
